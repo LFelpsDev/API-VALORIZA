8ba8244232c27c1829468edf6a603276f1d4026d
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-15 21:42:26 -0300

    Entendo Sobre Metodos, Criando nossas Primeras Rotas de Get e Post, Configurando o Insomina

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
 import express from 'express';
 
 // @types/express
-const app = express()
+const app = express();
+
+
+app.get('/testing', (request, response) => {
+  // Request => Entrando
+  // Response => Saindo
+  return response.send("Ola Mundo Diego Trem bala")
+})
+
+app.post('/test-post', (request, response) => {
+  return response.send('Ola mundo Nlw, Método Post')
+})
 
 // http://localhost:3000
 app.listen(3000, () => console.log('Server Running'))
