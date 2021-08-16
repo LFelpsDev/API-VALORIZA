23c27897996fddfc3bfe18750c905c998ada08b3
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-16 15:39:25 -0300

    Criando nosso Primeiro Service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,40 @@
+// tudo que for relacionado a criação de Usuários
+
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IUserRequest{
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest){
+    const usersRepository = new UsersRepositories();
+
+    if (!email){
+      throw new Error("Email Incorrect");
+    }
+
+    // Verificando se o Usuario Existe
+    const userAlreadyExists = await usersRepository.findOne({ email }); 
+
+    // se Existir Vou Forçar esse Erro
+    if (userAlreadyExists){
+      throw new Error("User Already Exists");
+    }
+
+    // Se tiver tudo certo cria essa instância, e salve esse objeto no banco de dados
+    const user = usersRepository.create({
+      name,
+      email,
+      admin
+    })
+
+    await usersRepository.save(user)
+
+    return user;
+  }
+}
+
+export { CreateUserService }
