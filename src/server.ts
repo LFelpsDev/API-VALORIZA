21164e93ac4d1766fb5a902881726610532c7a1a
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-16 17:50:54 -0300

    Arrumando Bugs Assincronos com a lib do express, e tratando o erro do terminal de Usuarios Existente

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata"
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors"
 import { router } from './routes'
 
 import "./database";
@@ -9,6 +10,19 @@ const app = express();
 app.use(express.json()); // por padrão ele pega vários tipos, e causa alguns erros na aplicação por isso, é preciso indicar o tipo que quero.
 
 app.use(router); // todas minhas rotas vão fazer parte do meu Projeto
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "Error",
+    message: "Internal Server Error"
+  })
+})
 
 // http://localhost:3000
 app.listen(3000, () => console.log('Server Running'))
