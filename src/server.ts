d2323b5984a06ccdafb214b31ffa213ff773489d
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-23 12:35:17 -0300

    Remove yarn.lock and package-json.lock, added cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import "reflect-metadata"
 import express, { Request, Response, NextFunction } from 'express';
 import "express-async-errors"
 import { router } from './routes'
+import cors from 'cors'
 
 import "./database";
 
 const app = express();
+app.use(cors())
 
 app.use(express.json()); // por padrão ele pega vários tipos, e causa alguns erros na aplicação por isso, é preciso indicar o tipo que quero.
 
